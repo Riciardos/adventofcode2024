f1c97621eb5af7ca68fe3d33b5cbc919ffcf9ba0
Ricardo Schuller <ricardoschuller90@gmail.com>
2024-12-04 00:19:15 +0100

    Day 1 star 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,6 @@
 mod day1 {}
 
+use std::collections::HashMap;
 use super::util::read_lines;
 
 pub fn day1() {
@@ -27,5 +28,35 @@ pub fn day1() {
         }
 
         println!("Part 1: {}", total_distance);
+    }
+}
+
+pub fn day1_star2() {
+    // read line by line
+    if let Ok(lines) = read_lines("src/day1/input.csv") {
+
+        let mut total_similarity = 0;
+        let mut left_array = Vec::<i32>::new();
+        let mut right_array = Vec::<i32>::new();
+
+        let numbers = lines;
+        for line in numbers {
+            if let Ok(data) = line {
+                let mut split = data.split_whitespace();
+                left_array.push(split.next().unwrap().parse::<i32>().unwrap());
+                right_array.push(split.next().unwrap().parse::<i32>().unwrap());
+            }
+        }
+
+        let occurences = right_array.iter().fold(HashMap::new(), |mut acc, x| {
+            *acc.entry(x).or_insert(0) += 1;
+            acc
+        });
+
+        for i in 0..left_array.len() {
+            total_similarity +=  left_array[i] * occurences.get(&(left_array[i])).unwrap_or(&0);
+        }
+
+        println!("Day 1 part 2: {}", total_similarity);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use crate::day1::day1;
+use crate::day1::{day1, day1_star2};
 
 mod day1;
 mod util;
 
 fn main() {
-    day1()
+    day1();
+    day1_star2();
 }
