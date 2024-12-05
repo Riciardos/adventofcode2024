a43759b7523fab0140067c58230c3b579380b7cd
Ricardo Schuller <ricardoschuller90@gmail.com>
2024-12-05 20:49:49 +0100

    Day 2 star 2

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,4 +1,5 @@
 use crate::util::read_lines;
+use std::cmp::Ordering;
 
 mod day5 {}
 
@@ -39,6 +40,61 @@ pub fn day5() {
             println!("Day 5 Part 1 : {}", total);
         }
     }
+}
+
+pub fn day5_star2() {
+    if let Ok(rules) = read_lines("src/day5/input_rules.txt") {
+        if let Ok(updates) = read_lines("src/day5/input_updates.txt") {
+            let rules_vec: Vec<(i32, i32)> = rules
+                .into_iter()
+                .map(|x| x.unwrap())
+                .map(|x| {
+                    let y = x.split_once("|").unwrap().clone();
+                    return (y.0.parse::<i32>().unwrap(), y.1.parse::<i32>().unwrap());
+                })
+                .collect();
+            let updates_vec: Vec<Vec<i32>> = updates
+                .into_iter()
+                .map(|x| {
+                    x.unwrap()
+                        .split(",")
+                        .map(|x| x.parse::<i32>().unwrap())
+                        .collect()
+                })
+                .collect();
+
+            let mut incorrect_updates = vec![];
+
+            updates_vec.iter().for_each(|update| {
+                if !check_rules(update, &rules_vec) {
+                    incorrect_updates.push(update.clone());
+                }
+            });
+
+            incorrect_updates.iter_mut().for_each(|update| {
+                bubble_sort_till_correct(update, &rules_vec);
+            });
+
+            let total = incorrect_updates.iter().fold(0, |acc, update| {
+                let middle_number = *update.get(update.len() / 2).unwrap();
+                acc + middle_number
+            });
+
+            println!("Day 5 Part 2 : {}", total);
+        }
+    }
+}
+
+fn bubble_sort_till_correct(update: &mut Vec<i32>, rules: &Vec<(i32, i32)>) {
+    update.sort_by(|a, b| {
+        if rules.iter().filter(|x| a.eq(&x.0) && b.eq(&x.1)).count() == 1 {
+            return Ordering::Less;
+        }
+        if rules.iter().filter(|x| b.eq(&x.0) && a.eq(&x.1)).count() == 1 {
+            return Ordering::Greater;
+        }
+        return Ordering::Equal;
+    });
 }
 
 fn check_rules(update: &Vec<i32>, rules: &Vec<(i32, i32)>) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::day1::{day1, day1_star2};
 use crate::day2::{day2, day2_star2};
 use crate::day3::{day3, day3_part2};
 use crate::day4::{day4, day4_star2};
-use crate::day5::day5;
+use crate::day5::{day5, day5_star2};
 
 mod day1;
 mod day2;
@@ -30,6 +30,7 @@ fn sequential() {
     day4();
     day4_star2();
     day5();
+    day5_star2();
 
     let elapsed = now.elapsed();
     println!("Elapsed sequential: {:.2?}", elapsed);
