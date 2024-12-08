ea3f7c0737bd80a4a57cc5a1fc81218d637a0c91
Ricardo Schuller <ricardoschuller90@gmail.com>
2024-12-08 17:17:18 +0100

    Day 7 part 2.

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,4 +1,3 @@
-use crate::day7::Operator::{Add, Multiply};
 use crate::util::read_lines;
 
 mod day7 {}
@@ -8,37 +7,52 @@ pub fn day7() {
         let mut correct_results = vec![];
         for string_result in lines {
             if let Ok(line) = string_result {
-                let (answer, operands) = line.split_once(": ")
+                let (answer, operands) = line
+                    .split_once(": ")
                     .map(|x| {
                         let answer = x.0.parse::<i64>().unwrap();
-                        let operands = x.1.split(" ").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
-                        return (answer, operands)
+                        let operands =
+                            x.1.split(" ")
+                                .map(|x| x.parse::<i64>().unwrap())
+                                .collect::<Vec<i64>>();
+                        return (answer, operands);
                     })
                     .unwrap();
 
-                if traverse_tree(answer, *operands.get(0).unwrap(), Add, &operands, 0 )
-                    || traverse_tree(answer, *operands.get(0).unwrap(), Multiply, &operands, 0 ) {
+                if traverse_tree(answer, *operands.get(0).unwrap(), &operands, 0)
+                {
                     correct_results.push(answer);
                 }
             }
         }
-        print!("Day 7 Part 1: {}\n", correct_results.iter().fold(0, |acc, answer| acc + answer));
+        print!(
+            "Day 7 Part 2: {}\n",
+            correct_results.iter().fold(0, |acc, answer| acc + answer)
+        );
     }
 }
 
-fn traverse_tree(answer: i64, val: i64, operator: Operator, operands: &Vec<i64>, depth: usize) -> bool {
+fn traverse_tree(
+    answer: i64,
+    val: i64,
+    operands: &Vec<i64>,
+    depth: usize,
+) -> bool {
     if depth == operands.len() {
-        return answer == match operator  {
-            Add => val + operands.get(depth).unwrap_or(&0),
-            Multiply => val * operands.get(depth).unwrap_or(&1)
-        }
+        return answer == val;
     }
-    let new_val_add = val + operands.get(depth + 1 ).unwrap_or(&0);
-    let new_val_mul = val * operands.get(depth + 1).unwrap_or(&1);
-    traverse_tree(answer, new_val_add,  Add,  operands, depth + 1) || traverse_tree(answer, new_val_mul, Multiply, operands, depth + 1)
-}
 
-enum Operator {
-    Multiply,
-    Add
+    let mut new_val_add = val;
+    let mut new_val_mul = val;
+    let mut new_val_concat = val;
+
+    if let Some(next_operand) = operands.get(depth + 1) {
+        new_val_add = val + next_operand;
+        new_val_mul = val * next_operand;
+        new_val_concat = (val.to_string() + &*next_operand.to_string()).parse::<i64>().unwrap();
+    }
+
+    traverse_tree(answer, new_val_add, operands, depth + 1)
+        || traverse_tree(answer, new_val_mul, operands, depth + 1)
+        || traverse_tree(answer, new_val_concat, operands, depth + 1)
 }
